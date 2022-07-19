a13fa77396ea376d90e56e84be7f6e4263dc57cb
RICHARD TANAKA <richard.tanaka@viavarejo.com.br>
2022-07-19 16:52:30 -0300

    Aula 02 - Gerando figurinhas para WhatsApp

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -6,25 +8,31 @@ import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
 
-
 public class App {
     public static void main(String[] args) throws Exception {
-        //fazer uma conexao http e buscar os top 250 filmes
+        // fazer uma conexao http e buscar os top 250 filmes
         String url = "https://imdb-api.com/en/API/Top250Movies/k_tbyhuvkg";
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(endereco).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        //extrair só os dados que interessam (título, poster, classificação)
+        // extrair só os dados que interessam (título, poster, classificação)
         var parser = new JsonParser();
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
-        System.out.println(listaDeFilmes.get(1));
-        //exibir e manipular os dados.
-        for (Map<String,String> filme : listaDeFilmes) {
-            System.out.println("Título: " + filme.get("title"));
-            System.out.println("Poster: " + filme.get("image"));           
-            System.out.println("\u001b[38;2;255;255;255m\u001b[48;2;42;122;228mClassificação: " + filme.get("rank") + "\u001b[m");
+        // exibir e manipular os dados.
+        for (Map<String, String> filme : listaDeFilmes) {
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = titulo + ".png";
+
+            var geradora = new GeradorDeFigurinhas();
+            geradora.cria(inputStream, nomeArquivo);
+
+            System.out.println(filme.get("title"));
+            System.out.println();
         }
     }
 }
